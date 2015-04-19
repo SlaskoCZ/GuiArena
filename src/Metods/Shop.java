ebf4c97bd53496df67484a9e40432c5726a8a898
Petr <Petr@Petr-PC>
2015-04-19 17:48:09 +0200

    added gettings and settings

--- src/Metods/Shop.java
+++ src/Metods/Shop.java
@@ -25,12 +25,13 @@ import java.io.Writer;
 
 public class Shop {
 
-    public static String[][] lines;
+    private static String[][] inventory;
+    private static String itemType;
 
     void buy() throws IOException {
         Utilities utilities = new Utilities();
         Inputs input = new Inputs();
-        for (int i = 1; i < (lines.length - 1); i++) {
+        for (int i = 1; i < (inventory.length - 1); i++) {
             if (i < 10) {
                 System.out.print("  ");
             } else if (i < 100) {
@@ -38,8 +39,8 @@ public class Shop {
             }
             System.out.print(i);
             System.out.print(". ");
-            for (int j = 1; j < lines[i].length; j++) {
-                System.out.print(lines[i][j]);
+            for (int j = 1; j < inventory[i].length; j++) {
+                System.out.print(inventory[i][j]);
             }
             System.out.println("");
         }
@@ -48,91 +49,91 @@ public class Shop {
         System.out.println("");
         String text = "Do you want to buy: "
                 + //Item name
-                lines[selection][1]
+                inventory[selection][1]
                 + //Item Strenght
                 " STR: "
-                + lines[selection][2]
+                + inventory[selection][2]
                 + //Item Dexterity
                 " DEX: "
-                + lines[selection][3]
+                + inventory[selection][3]
                 + //Item Vitality
                 " VIT: "
-                + lines[selection][4]
+                + inventory[selection][4]
                 + //Item Inteligence
                 " INT: "
-                + lines[selection][5]
+                + inventory[selection][5]
                 + //Item damage
                 " DMG: "
-                + lines[selection][6]
+                + inventory[selection][6]
                 + //Item damage
                 " DEF: "
-                + lines[selection][7]
+                + inventory[selection][7]
                 + //Item price
                 " For: "
-                + lines[selection][9]
+                + inventory[selection][9]
                 + "gold ? [Yes/No]";
         System.out.println(text);
         switch (input.input()) {
             case "Yes":
 
-                int line = getLines(Utilities.temp.getAbsolutePath());
+                int line = getLines(Utilities.getTemp().getAbsolutePath());
                 String[] load = new String[line + 1];
-                if (Integer.valueOf(lines[selection][9].trim()) > HeroStats.money) {
+                if (Integer.valueOf(inventory[selection][9].trim()) > HeroStats.getMoney()) {
                     System.out.println("You don't have enought money!");
                     utilities.waitForEnter();
                     utilities.clearScreen();
                 } else {
-                    System.out.println("Sucessfuly bought " + lines[selection][1]);
+                    System.out.println("Sucessfuly bought " + inventory[selection][1]);
 
-                    FileReader fr = new FileReader(Utilities.temp);
+                    FileReader fr = new FileReader(Utilities.getTemp());
                     BufferedReader br = new BufferedReader(fr);
-                    HeroStats.money -= Integer.valueOf(lines[selection][9]);
+                    HeroStats.setMoney(-Integer.valueOf(inventory[selection][9]));
 
-                    for (int i = 0; i < HeroStats.inventoryItems; i++) {
+                    for (int i = 0; i < HeroStats.getInventoryItems(); i++) {
                         load[i] = br.readLine();
 
                     }
-                    Utilities.temp.delete();
-                    Utilities.temp.createNewFile();
-                    FileWriter fw = new FileWriter(Utilities.temp);
+                    Utilities.getTemp().delete();
+                    Utilities.getTemp().createNewFile();
+                    FileWriter fw = new FileWriter(Utilities.getTemp());
                     BufferedWriter bw = new BufferedWriter(fw);
                     Writer w = new BufferedWriter(bw);
                     String[] work;
-                    for (int i = 0; i < HeroStats.inventoryItems; i++) {
+                    for (int i = 0; i < HeroStats.getInventoryItems(); i++) {
                         work = load[i].split(";");
                         w.write(i + ";" + work[1] + ";" + work[2] + ";" + work[3] + ";" + work[4] + ";" + work[5] + ";" + work[6] + ";" + work[7] + ";" + work[8] + ";" + work[9]);
                         w.write(System.lineSeparator());
                     }
-                    HeroStats.inventoryItems += 1;
+                    HeroStats.setInventoryItems(HeroStats.getInventoryItems()+1);
                     w.write(line
                             +//item name
                             ";"
-                            + lines[selection][1]
+                            + inventory[selection][1]
                             + //Item Strenght
                             ";"
-                            + lines[selection][2]
+                            + inventory[selection][2]
                             + //Item Dexterity
                             ";"
-                            + lines[selection][3]
+                            + inventory[selection][3]
                             + //Item Vitality
                             ";"
-                            + lines[selection][4]
+                            + inventory[selection][4]
                             + //Item Inteligence
                             ";"
-                            + lines[selection][5]
+                            + inventory[selection][5]
                             + //Item damage                           
                             ";"
-                            + lines[selection][6]
+                            + inventory[selection][6]
                             + //Item defense                          
                             ";"
-                            + lines[selection][7]
+                            + inventory[selection][7]
                             + //Item Type
                             ";"
-                            + lines[selection][8]
+                            + inventory[selection][8]
                             + //Item price
                             ";"
-                            + (Integer.valueOf(lines[selection][9]) / 2));
-                    System.out.println(Utilities.temp);
+                            + (Integer.valueOf(inventory[selection][9]) / 2));
+                    System.out.println(Utilities.getTemp());
                     w.close();
                     fw.close();
                     bw.close();
@@ -144,39 +145,39 @@ public class Shop {
 
     }
 
-    void loadShop(String type) throws FileNotFoundException, IOException {
-        String path = "./resource/files/" + type + ".txt";
+    void loadShop() throws FileNotFoundException, IOException {
+        String path = "./resource/files/" + getItemType() + ".txt";
         int numberOfLines = getLines(path);
         BufferedReader bufferedReader;
         FileReader fileReader = new FileReader(path);
         bufferedReader = new BufferedReader(fileReader);
         int i = 1;
-        lines = new String[numberOfLines + 1][10];
+        inventory = new String[numberOfLines + 1][10];
         while (i < numberOfLines) {
             String readed = bufferedReader.readLine();
             if (readed.contains("##")) {
             } else {
                 String[] part = readed.split(";");
                 //Item ID
-                lines[i][0] = part[0];
+                inventory[i][0] = part[0];
                 //Item name
-                lines[i][1] = part[1];
+                inventory[i][1] = part[1];
                 //Item Strenght
-                lines[i][2] = part[2];
+                inventory[i][2] = part[2];
                 //Item Dexterity
-                lines[i][3] = part[3];
+                inventory[i][3] = part[3];
                 //Item Vitality
-                lines[i][4] = part[4];
+                inventory[i][4] = part[4];
                 //Item Inteligence
-                lines[i][5] = part[5];
+                inventory[i][5] = part[5];
                 //Item damage
-                lines[i][6] = part[6];
+                inventory[i][6] = part[6];
                 //Item defense
-                lines[i][7] = part[7];
+                inventory[i][7] = part[7];
                 //Item Type
-                lines[i][8] = part[8];
+                inventory[i][8] = part[8];
                 //Item price
-                lines[i][9] = part[9];
+                inventory[i][9] = part[9];
 
                 i++;
             }
@@ -205,15 +206,15 @@ public class Shop {
     }
 
     void sell() throws FileNotFoundException, IOException {
-        FileReader fileReader = new FileReader(Utilities.temp);
+        FileReader fileReader = new FileReader(Utilities.getTemp());
         BufferedReader bufferedReader = new BufferedReader(fileReader);
-        int numberOfItems = getLines(Utilities.temp.getPath());
+        int numberOfItems = getLines(Utilities.getTemp().getPath());
         String[] items = new String[numberOfItems];
         if (numberOfItems == 0) {
             System.out.println("Nothing to sell in backpack.");
 
         } else {
-            for (int i = 1; i < HeroStats.inventoryItems; i++) {
+            for (int i = 1; i < HeroStats.getInventoryItems(); i++) {
                 String readed = bufferedReader.readLine();
                 System.out.println(readed);
                 items[i] = readed;
@@ -222,4 +223,22 @@ public class Shop {
             bufferedReader.close();
         }
     }
+
+    public static String[][] getInventory() {
+        return inventory;
+    }
+
+    public static void setLines(int Index1, int Index2, String lines) {
+        Shop.inventory[Index1][Index2] = lines;
+    }
+
+    public static String getItemType() {
+        return itemType;
+    }
+
+    public static void setItemType(String itemType) {
+        Shop.itemType = itemType;
+    }
+    
+    
 }
