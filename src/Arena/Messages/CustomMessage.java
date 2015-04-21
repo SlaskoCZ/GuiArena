97f9336d8ec7c41c3ade07a871009d40d5e90653
Petr <Petr@Petr-PC>
2015-04-21 18:05:47 +0200

    switched label for TextField

--- src/Arena/Messages/CustomMessage.java
+++ src/Arena/Messages/CustomMessage.java
@@ -20,7 +20,7 @@ public class CustomMessage extends javax.swing.JDialog {
         super(parent, modal);
         initComponents();
         this.setTitle(title);
-        customMessageLabel.setText(message);
+        customMessageTextFieldArea.setText(message);
     }
 
     /**
@@ -32,12 +32,12 @@ public class CustomMessage extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        customMessageLabel = new javax.swing.JLabel();
         customMessageButtonOK = new javax.swing.JButton();
+        customMessageTextField = new javax.swing.JScrollPane();
+        customMessageTextFieldArea = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setAlwaysOnTop(true);
-        setPreferredSize(new java.awt.Dimension(200, 100));
         setResizable(false);
 
         customMessageButtonOK.setText("OK");
@@ -48,24 +48,37 @@ public class CustomMessage extends javax.swing.JDialog {
             }
         });
 
+        customMessageTextFieldArea.setEditable(false);
+        customMessageTextFieldArea.setBackground(new java.awt.Color(240, 240, 240));
+        customMessageTextFieldArea.setColumns(3);
+        customMessageTextFieldArea.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        customMessageTextFieldArea.setLineWrap(true);
+        customMessageTextFieldArea.setRows(3);
+        customMessageTextFieldArea.setWrapStyleWord(true);
+        customMessageTextFieldArea.setAutoscrolls(false);
+        customMessageTextFieldArea.setBorder(null);
+        customMessageTextFieldArea.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        customMessageTextFieldArea.setMargin(new java.awt.Insets(0, 0, 0, 0));
+        customMessageTextField.setViewportView(customMessageTextFieldArea);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(75, 75, 75)
-                .addComponent(customMessageButtonOK, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(75, Short.MAX_VALUE))
-            .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(customMessageLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(customMessageTextField)
                 .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(98, 98, 98)
+                .addComponent(customMessageButtonOK, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(102, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(customMessageLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 42, Short.MAX_VALUE)
+                .addComponent(customMessageTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 68, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(customMessageButtonOK, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
@@ -122,6 +135,7 @@ public class CustomMessage extends javax.swing.JDialog {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton customMessageButtonOK;
-    private javax.swing.JLabel customMessageLabel;
+    private javax.swing.JScrollPane customMessageTextField;
+    private javax.swing.JTextArea customMessageTextFieldArea;
     // End of variables declaration//GEN-END:variables
 }
