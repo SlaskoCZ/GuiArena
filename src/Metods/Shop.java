86a52fd4cc94a2721430f2718c8f4a517b637f70
Petr <Petr@Petr-PC>
2015-04-21 18:07:16 +0200

    changed buy Methode

--- src/Metods/Shop.java
+++ src/Metods/Shop.java
@@ -31,122 +31,175 @@ public class Shop {
     private static String[][] inventory;
     private static String itemType;
     private static DefaultListModel listModel;
+    // <editor-fold defaultstate="collapsed" desc="Old Buy()">
 
-    void buy() throws IOException {
-        Utilities utilities = new Utilities();
-        Inputs input = new Inputs();
-        for (int i = 1; i < (inventory.length - 1); i++) {
-            if (i < 10) {
-                System.out.print("  ");
-            } else if (i < 100) {
-                System.out.print(" ");
-            }
-            System.out.print(i);
-            System.out.print(". ");
-            for (int j = 1; j < inventory[i].length; j++) {
-                System.out.print(inventory[i][j]);
-            }
-            System.out.println("");
-        }
-        System.out.print("Choose what do you want to buy: ");
-        int selection = Integer.valueOf(input.input());
-        System.out.println("");
-        String text = "Do you want to buy: "
-                + //Item name
-                inventory[selection][1]
-                + //Item Strenght
-                " STR: "
-                + inventory[selection][2]
-                + //Item Dexterity
-                " DEX: "
-                + inventory[selection][3]
-                + //Item Vitality
-                " VIT: "
-                + inventory[selection][4]
-                + //Item Inteligence
-                " INT: "
-                + inventory[selection][5]
-                + //Item damage
-                " DMG: "
-                + inventory[selection][6]
-                + //Item damage
-                " DEF: "
-                + inventory[selection][7]
-                + //Item price
-                " For: "
-                + inventory[selection][9]
-                + "gold ? [Yes/No]";
-        System.out.println(text);
-        switch (input.input()) {
-            case "Yes":
-
-                int line = getLines(Utilities.getTemp().getAbsolutePath());
-                String[] load = new String[line + 1];
-                if (Integer.valueOf(inventory[selection][9].trim()) > HeroStats.getMoney()) {
-                    System.out.println("You don't have enought money!");
-                    utilities.waitForEnter();
-                    utilities.clearScreen();
+//    void buy() {
+//        Utilities utilities = new Utilities();
+//        Inputs input = new Inputs();
+//        for (int i = 1; i < (inventory.length - 1); i++) {
+//            if (i < 10) {
+//                System.out.print("  ");
+//            } else if (i < 100) {
+//                System.out.print(" ");
+//            }
+//            System.out.print(i);
+//            System.out.print(". ");
+//            for (int j = 1; j < inventory[i].length; j++) {
+//                System.out.print(inventory[i][j]);
+//            }
+//            System.out.println("");
+//        }
+//        System.out.print("Choose what do you want to buy: ");
+//        int selection = Integer.valueOf(input.input());
+//        System.out.println("");
+//        String text = "Do you want to buy: "
+//                + //Item name
+//                inventory[selection][1]
+//                + //Item Strenght
+//                " STR: "
+//                + inventory[selection][2]
+//                + //Item Dexterity
+//                " DEX: "
+//                + inventory[selection][3]
+//                + //Item Vitality
+//                " VIT: "
+//                + inventory[selection][4]
+//                + //Item Inteligence
+//                " INT: "
+//                + inventory[selection][5]
+//                + //Item damage
+//                " DMG: "
+//                + inventory[selection][6]
+//                + //Item damage
+//                " DEF: "
+//                + inventory[selection][7]
+//                + //Item price
+//                " For: "
+//                + inventory[selection][9]
+//                + "gold ? [Yes/No]";
+//        System.out.println(text);
+//        switch (input.input()) {
+//            case "Yes":
+//
+//                int line = 0;
+//                try {
+//                    line = getLines(Utilities.getTemp().getAbsolutePath());
+//                } catch (IOException ex) {
+//                    Logger.getLogger(Shop.class.getName()).log(Level.SEVERE, null, ex);
+//                }
+//                String[] load = new String[line + 1];
+//                if (Integer.valueOf(inventory[selection][9].trim()) > HeroStats.getMoney()) {
+//                    System.out.println("You don't have enought money!");
+//
+//                } else {
+//                    System.out.println("Sucessfuly bought " + inventory[selection][1]);
+//
+//                    FileReader fr = null;
+//                    try {
+//                        fr = new FileReader(Utilities.getTemp());
+//                    } catch (FileNotFoundException ex) {
+//                        Logger.getLogger(Shop.class.getName()).log(Level.SEVERE, null, ex);
+//                    }
+//                    BufferedReader br = new BufferedReader(fr);
+//                    HeroStats.setMoney(-Integer.valueOf(inventory[selection][9]));
+//
+//                    for (int i = 0; i < HeroStats.getInventoryItems(); i++) {
+//                        try {
+//                            load[i] = br.readLine();
+//                        } catch (IOException ex) {
+//                            Logger.getLogger(Shop.class.getName()).log(Level.SEVERE, null, ex);
+//                        }
+//
+//                    }
+//                    Utilities.getTemp().delete();
+//                    try {
+//                        Utilities.getTemp().createNewFile();
+//                    } catch (IOException ex) {
+//                        Logger.getLogger(Shop.class.getName()).log(Level.SEVERE, null, ex);
+//                    }
+//                    FileWriter fw = null;
+//                    try {
+//                        fw = new FileWriter(Utilities.getTemp());
+//                    } catch (IOException ex) {
+//                        Logger.getLogger(Shop.class.getName()).log(Level.SEVERE, null, ex);
+//                    }
+//                    BufferedWriter bw = new BufferedWriter(fw);
+//                    Writer w = new BufferedWriter(bw);
+//                    String[] work;
+//                    for (int i = 0; i < HeroStats.getInventoryItems(); i++) {
+//                        work = load[i].split(";");
+//                        try {
+//                            w.write(i + ";" + work[1] + ";" + work[2] + ";" + work[3] + ";" + work[4] + ";" + work[5] + ";" + work[6] + ";" + work[7] + ";" + work[8] + ";" + work[9]);
+//                            w.write(System.lineSeparator());
+//                            HeroStats.setInventoryItems(HeroStats.getInventoryItems() + 1);
+//                            w.write(line
+//                                    +//item name
+//                                    ";"
+//                                    + inventory[selection][1]
+//                                    + //Item Strenght
+//                                    ";"
+//                                    + inventory[selection][2]
+//                                    + //Item Dexterity
+//                                    ";"
+//                                    + inventory[selection][3]
+//                                    + //Item Vitality
+//                                    ";"
+//                                    + inventory[selection][4]
+//                                    + //Item Inteligence
+//                                    ";"
+//                                    + inventory[selection][5]
+//                                    + //Item damage                           
+//                                    ";"
+//                                    + inventory[selection][6]
+//                                    + //Item defense                          
+//                                    ";"
+//                                    + inventory[selection][7]
+//                                    + //Item Type
+//                                    ";"
+//                                    + inventory[selection][8]
+//                                    + //Item price
+//                                    ";"
+//                                    + (Integer.valueOf(inventory[selection][9]) / 2));
+//                            System.out.println(Utilities.getTemp());
+//                        } catch (IOException ex) {
+//                            Logger.getLogger(Shop.class.getName()).log(Level.SEVERE, null, ex);
+//                        }
+//
+//                    }
+//
+//                    try {
+//                        w.close();
+//                        fw.close();
+//                        bw.close();
+//                        fr.close();
+//                        br.close();
+//                    } catch (IOException ex) {
+//                        Logger.getLogger(Shop.class.getName()).log(Level.SEVERE, null, ex);
+//                    }
+//
+//                }
+//
+//        }
+//
+//    }
+// </editor-fold> 
+    public static void buy(int itemIndex) {
+        System.out.println("Cena: "+inventory[itemIndex][9]);
+        System.out.println("Aktualne: "+HeroStats.getMoney());
+        if (Integer.valueOf(inventory[itemIndex][9]) <= HeroStats.getMoney()) {
+            int j = 0;
+            for (int i = 0; i < inventory[itemIndex].length; i++) {
+                
+                if (i != 8 && i != 0) {
+                    HeroStats.setHeroInventory(HeroStats.getInventoryItems(), j, inventory[itemIndex][i]);
+                    
                 } else {
-                    System.out.println("Sucessfuly bought " + inventory[selection][1]);
-
-                    FileReader fr = new FileReader(Utilities.getTemp());
-                    BufferedReader br = new BufferedReader(fr);
-                    HeroStats.setMoney(-Integer.valueOf(inventory[selection][9]));
-
-                    for (int i = 0; i < HeroStats.getInventoryItems(); i++) {
-                        load[i] = br.readLine();
-
-                    }
-                    Utilities.getTemp().delete();
-                    Utilities.getTemp().createNewFile();
-                    FileWriter fw = new FileWriter(Utilities.getTemp());
-                    BufferedWriter bw = new BufferedWriter(fw);
-                    Writer w = new BufferedWriter(bw);
-                    String[] work;
-                    for (int i = 0; i < HeroStats.getInventoryItems(); i++) {
-                        work = load[i].split(";");
-                        w.write(i + ";" + work[1] + ";" + work[2] + ";" + work[3] + ";" + work[4] + ";" + work[5] + ";" + work[6] + ";" + work[7] + ";" + work[8] + ";" + work[9]);
-                        w.write(System.lineSeparator());
-                    }
-                    HeroStats.setInventoryItems(HeroStats.getInventoryItems() + 1);
-                    w.write(line
-                            +//item name
-                            ";"
-                            + inventory[selection][1]
-                            + //Item Strenght
-                            ";"
-                            + inventory[selection][2]
-                            + //Item Dexterity
-                            ";"
-                            + inventory[selection][3]
-                            + //Item Vitality
-                            ";"
-                            + inventory[selection][4]
-                            + //Item Inteligence
-                            ";"
-                            + inventory[selection][5]
-                            + //Item damage                           
-                            ";"
-                            + inventory[selection][6]
-                            + //Item defense                          
-                            ";"
-                            + inventory[selection][7]
-                            + //Item Type
-                            ";"
-                            + inventory[selection][8]
-                            + //Item price
-                            ";"
-                            + (Integer.valueOf(inventory[selection][9]) / 2));
-                    System.out.println(Utilities.getTemp());
-                    w.close();
-                    fw.close();
-                    bw.close();
-                    fr.close();
-                    br.close();
+                    j--;
                 }
-
+                j++;
+            }
+            HeroStats.setInventoryItems(1);
         }
-
     }
 
     public static void loadShop() {
@@ -234,15 +287,17 @@ public class Shop {
             bufferedReader.close();
         }
     }
-    public static void removeInventory(){
+
+    public static void removeInventory() {
         inventory = new String[0][0];
     }
-    public static String[] getItemStats(int index){
+
+    public static String[] getItemStats(int index) {
         String[] item = new String[10];
         System.arraycopy(inventory[index], 0, item, 0, inventory[index].length);
         return item;
     }
-    
+
     public static String[][] getInventory() {
         return inventory;
     }
@@ -268,7 +323,7 @@ public class Shop {
 
     public static void getItemNames() {
         listModel = new DefaultListModel();
-        for (int i = 1; i < inventory.length-1; i++) {
+        for (int i = 1; i < inventory.length - 1; i++) {
             listModel.addElement(getInventory()[i][1].trim());
         }
 
